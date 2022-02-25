957748b2529966c3fa7f378ea9bb4b6f5d6b0dba
Neha Dhyani <nehadhyani301@gmail.com>
2022-02-25 19:39:17 +0530

    minor changes

--- frontend/src/AddTask.js
+++ frontend/src/AddTask.js
@@ -25,14 +25,12 @@ const AddTask = (props) => {
     }
     setInput('');
   }
-  
-  const newTaskStyle = {background: 'transparent', color: 'white'};
 
   return (
     <div className="container mt-3 mb-4">
       <div className="row justify-content-center">
         <div className="col-9 col-sm-10 col-md-11 p-2">
-          <input className="form-control newTask" style= {newTaskStyle} type="text" placeholder="New Task" value={input} onChange={handleChange}/>
+          <input className="form-control newTask" type="text" placeholder="New Task" value={input} onChange={handleChange}/>
         </div>
         <div className="col-2 col-sm-2 col-md-1 my-2 px-0">
           <Button className="btn addButton" variant="outline-secondary" onClick={addTasks}>Add</Button>
